991fcb0634ed9a0c8463ee13ecca58e829e5924d
Shailya777 <shailya.gandhi100@gmail.com>
2026-05-27 12:00:59 +0530

    Generating SQL from User Query and Displaying it for varification

--- src/ui/app.py
+++ src/ui/app.py
@@ -27,4 +27,20 @@ with st.sidebar:
     user_query= st.text_area('Ask a business question:',
                              placeholder= 'e.g., What is our total revenue by product category?')
     run_button= st.button('Generate Insights',
-                          type= 'secondary')
+                          type= 'primary')
+
+# Main Execution Logic:
+if run_button and user_query:
+    with st.spinner('Analyzing request and generating SQL..'):
+
+        # Calling SQL Engine:
+        result= execute_text_to_sql(user_query)
+        df = result.get('dataframe')
+        metadata= result.get('metadata')
+
+        # Displaying Result and Chart:
+        if df is not None and not df.empty and metadata is not None:
+            # Displaying Generated SQL for Varification:
+            with st.expander('🔍 View AI-Generated SQL', expanded= False):
+                st.code(metadata.sql_query, language= 'sql')
+                st.caption(f'Chart logic selected: {metadata.chart_type.upper()} | X: {metadata.x_axis} | Y: {metadata.y_axis}')
