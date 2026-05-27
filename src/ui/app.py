b4e3187120efb99e011ea8b10b80b346781bb373
Shailya777 <shailya.gandhi100@gmail.com>
2026-05-27 11:44:50 +0530

    Page config and title

--- src/ui/app.py
+++ src/ui/app.py
@@ -10,3 +10,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')
 # Importing SQL Engine Module:
 from src.engines.sql_engine import execute_text_to_sql
 
+# Page Config:
+st.set_page_config(
+    page_title= 'Agentic Data Hub',
+    page_icon= '📊',
+    layout= 'wide',
+)
+
+st.title('📊 Agentic Data Intelligence Hub')
+st.markdown('Ask natural language questions about your e-commerce data. It will get converted to SQL, and you can verify the logic before trusting the results.')
+st.divider()
