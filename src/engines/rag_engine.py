253fb81680cc92f5b66ce1e7e6dbb237d08160f5
Shailya777 <shailya.gandhi100@gmail.com>
2026-05-28 11:50:05 +0530

    Embeddings and ChromaDB Collection

--- src/engines/rag_engine.py
+++ src/engines/rag_engine.py
@@ -14,3 +14,15 @@ openai= OpenAI(api_key= OPENAI_API_KEY)
 DB_PATH= os.path.abspath(os.path.join(os.path.dirname(__file__),"../../data/vector_db"))
 #print(DB_PATH)
 chroma_client= chromadb.PersistentClient(path= DB_PATH)
+
+# Embedding:
+embedding= embedding_functions.OpenAIEmbeddingFunction(
+    api_key= OPENAI_API_KEY,
+    model_name= 'text_embedding-3-small'
+)
+
+# Get or Create Collection in Vector DB:
+collection= chroma_client.get_or_create_collection(
+    name= 'customer_reviews',
+    embedding_function= embedding
+)
