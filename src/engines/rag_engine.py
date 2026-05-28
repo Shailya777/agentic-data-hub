c1b28e3dcd085ba1978949aedf85216575be9e3e
Shailya777 <shailya.gandhi100@gmail.com>
2026-05-28 12:00:04 +0530

    Embedding Reviews and Metadata and Storing it into VectorDB.

--- src/engines/rag_engine.py
+++ src/engines/rag_engine.py
@@ -44,3 +44,27 @@ def build_vector_database(csv_path: str, sample_size: int= 1000):
 
     # Sampling sample_size reviews from data:
     df= df.tail(sample_size).reset_index(drop= True)
+
+    # Embedding reviews and Storing it in Vector Store:
+    documents= []
+    metadatas= []
+    ids= []
+    print(f'Embedding {len(df)} reviews into ChromaDB...')
+    for index, row in df.iterrows():
+        # Combining Title and Message if both exists:
+        title= str(row['review_comment_title']) if pd.notna(row['review_comment_title']) else ''
+        message= str(row['review_comment_message'])
+        full_text= f"Title: {title}\nMessage: {message}".strip()
+
+        documents.append(full_text)
+        metadatas.append({'score': int(row['review_score']), 'review_id': str(row['review_id'])})
+        ids.append(f"doc_{index}")
+
+    # Adding to ChromaDB:
+    collection.add(
+        documents= documents,
+        metadatas= metadatas,
+        ids= ids
+    )
+
+    print('Vector database Successfully Populated!')
